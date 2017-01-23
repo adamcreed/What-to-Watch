31cf08db3c7b07c34a118f1daf432116cddf1da4
Adam Reed <kusa.xisa@gmail.com>
2017-01-22 23:06:10 -0500

    Added tests

--- spec/movie_main_spec.rb
+++ spec/movie_main_spec.rb
@@ -1,61 +1,86 @@
 require 'movie_main'
 
-describe 'create_movie_lists' do
-  it 'creates movie list by id with movie id and title' do
-    #Arrange
-    #Act
-    movies_by_id, movies_by_title = create_movie_lists
-    #Assert
-    expect(movies_by_id[1].title).to eq "Toy Story (1995)"
-    expect(movies_by_title.has_key?('Toy Story (1995)')).to eq true
-
+describe 'movie lookup' do
+  before :all do
+    @movies_by_id, @movies_by_title = create_movie_lists
+    @users = create_user_list
+    @movies_by_id, @users = get_ratings(@movies_by_id, @users)
+    @rankings_list = create_movie_rankings(@movies_by_id)
   end
-end
 
-describe 'create_user_list' do
-  it 'creates a list of movielens users' do
-    #Arrange
-    #Act
-    users = create_user_list
-    #Assert
-    expect(users.size).to eq 943
+  describe '#create_movie_lists' do
+    it 'creates movie list by id with movie id and title' do
+      expect(@movies_by_id[1].title).to eq "Toy Story (1995)"
+      expect(@movies_by_title.has_key?('Toy Story (1995)')).to eq true
+    end
   end
-end
 
-describe 'get_ratings' do
-  it 'reads ratings from the reviews file and assigns them to movie and user files' do
-    movies_by_id, movies_by_title = create_movie_lists
-    users = create_user_list
-
-    movies_by_id, users = get_ratings(movies_by_id, users)
-
-    expect(movies_by_id[1].ratings.empty?).to eq false
-    expect(users[10].ratings.empty?).to eq false
+  describe '#create_user_list' do
+    it 'creates a list of movielens users' do
+      expect(@users.size).to eq 943
+    end
   end
-end
 
-describe 'movie_rankings' do
-  it 'creates list (array) of all movies with their id and average rating' do
-    movies_by_id, movies_by_title = create_movie_lists
-    users = create_user_list
-
-    movies_by_id, users = get_ratings(movies_by_id, users)
-    rankings_list = create_movie_rankings(movies_by_id)
-
-    expect(rankings_list[0][1]).to eq 408
+  describe '#get_ratings' do
+    it 'reads ratings from the reviews file and assigns them to movie and user files' do
+      expect(@movies_by_id[1].ratings.empty?).to eq false
+      expect(@users[10].ratings.empty?).to eq false
+    end
   end
-end
 
-describe 'top_ten_recommendations' do
-  it "gives a list of the top ten movies that a user hasn't seen" do
-    #Arrange
-    movies_by_id, movies_by_title = create_movie_lists
-    users = create_user_list
-    movies_by_id, users = get_ratings(movies_by_id, users)
-    rankings_list = create_movie_rankings(movies_by_id)
-    #Act
-    recommendations = top_ten_recommendations(users[1], rankings_list)
-    #Assert
-    expect(recommendations.size).to eq 10
+  describe '#movie_rankings' do
+    it 'creates list (array) of all movies with their id and average rating' do
+      expect(@rankings_list[0][1]).to eq 483
+    end
+  end
+
+  describe '#top_ten_recommendations' do
+    it "gives a list of the top ten movies that a user hasn't seen" do
+      recommendations = top_ten_recommendations(@users[1], @rankings_list)
+
+      expect(recommendations.size).to eq 10
+    end
+  end
+
+  describe '#user_chose_lookup?' do
+    it 'checks which option was chosen' do
+      expect(user_chose_lookup?('1')).to eq true
+    end
+  end
+
+  describe '#search_movies' do
+    it 'looks for an entered string within the movie list' do
+      movie_list = search_movies(@movies_by_title, 'star wars')
+
+      expect(movie_list.keys).to eq ['Star Wars (1977)']
+    end
+  end
+
+  describe '#is_int?' do
+    context 'when a number is entered' do
+      it 'checks if the text entered is an integer' do
+        expect(is_int?('1')).to eq true
+      end
+    end
+
+    context 'when a letter is entered' do
+      it 'checks if the text entered is an integer' do
+        expect(is_int?('s')).to eq false
+      end
+    end
+  end
+
+  describe '#is_within_bounds?' do
+    context 'when a valid number is entered' do
+      it 'checks if a number falls within a given range' do
+        expect(is_within_bounds?(2, 1..2)).to eq true
+      end
+    end
+
+    context 'when an invalid number is entered' do
+      it 'checks if a number falls within a given range' do
+        expect(is_within_bounds?(3, 1..2)).to eq false
+      end
+    end
   end
 end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,36 +1,33 @@
 require 'movie'
 
 describe Movie do
+  before :all do
+    @movie = Movie.new(movie_id: 1, title: 'Toy Story')
+    @movie.ratings = {1 => 3, 2 => 4, 3 => 5}
+  end
+
   describe '#new' do
     it 'it creates a movie with an id, title and rating' do
-      # Arrange
-      movie = Movie.new(movie_id: 1, title: 'Toy Story')
-      # Act
-      # Assert
-      expect(movie.movie_id).to eq 1
-      expect(movie.title).to eq 'Toy Story'
+      expect(@movie.movie_id).to eq 1
+      expect(@movie.title).to eq 'Toy Story'
     end
   end
 
   describe '#ratings' do
     it 'gives the list of individual ratings' do
-      # Arrange
-      movie = Movie.new(movie_id: 1, title: 'Toy Story')
-      #Act
-      movie.ratings = {1 => 3, 2 => 4, 3 => 5}
-      #Assert
-      expect(movie.ratings).to eq({1 => 3, 2 => 4, 3 => 5})
+      expect(@movie.ratings).to eq({1 => 3, 2 => 4, 3 => 5})
     end
   end
 
-  describe "#average_rating" do
-    it "gives the average rating for a movie" do
-      #Arrange
-      movie = Movie.new(movie_id: 1, title: 'Toy Story')
-      #Act
-      movie.ratings = {1 => 3, 2 => 4, 3 => 5}
-      #Assert
-      expect(movie.average_rating).to eq 4
+  describe '#average_rating' do
+    it 'gives the average rating for a movie' do
+      expect(@movie.average_rating).to eq 4
+    end
+  end
+
+  describe '#to_s' do
+    it 'gives the title and average rating of a movie' do
+      expect(@movie.to_s).to eq "Title: Toy Story\nRating: 4.0"
     end
   end
 end
